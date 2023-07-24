b1349189507b689459f4c68c83f6cbc0e2ca0885
De <spark49942@gmail.com>
2023-07-24 20:00:53 +0300

    DZ-2-infdev

--- dz.py
+++ dz.py
@@ -1,9 +1,7 @@
 import os
 from mod_convert import conveyor
-import sys
-sys.setrecursionlimit(10000)
 
-def recycle(direct, dpath = ""):
+def recycle(direct, dpath = ''):
     path = os.path.abspath(direct).split('\\')
     for d in range(len(path)-1):
         dpath = dpath + path[d] + '\\'
@@ -19,24 +17,24 @@ def disize(cat, score = 0):
             score += disize(get)
     return score
 
-def revercats(direct, dpath = "", d = 0):
+def revercats(direct, dpath = '', d = 0):
     path = os.path.abspath(direct).split('\\')
-    path.pop(-1)
+    path.pop()
     if d != len(path):
         return revercats(direct, dpath + path[d] + '\\', d + 1)
     if d >= path.index(direct):
-        momsi = str(disize(dpath)) + " bytes"
-        conveyor("Parent directory", path[path.index(direct)-1])
-        conveyor(path[path.index(direct)], momsi)
+        momsi = str(disize(dpath)) + ' bytes'
+        conveyor('Parent directory', path[path.index(direct)-1])
+        conveyor('Chosen directory ' + str(path[path.index(direct)]), momsi)
         for get in os.listdir(dpath):
             if os.path.isfile(get):
                 filesi = 'file ' + str(os.path.getsize(get)) + ' bytes'
                 conveyor(get, filesi)
             elif os.path.isdir(get):
-                dicsi = 'directory ' + str(disize(get)) + " bytes"
+                dicsi = 'directory ' + str(disize(get)) + ' bytes'
                 conveyor(get, dicsi)
 
-direct = input("2. Введите название желаемой конечной директории\n: ")
+direct = input('2. Введите название желаемой конечной директории\n: ')
 recycle(direct)
 revercats(direct)
-print("Выполнено!")
+print('Выполнено!')
--- mod_convert.py
+++ mod_convert.py
@@ -3,17 +3,17 @@ import json
 import pickle
 
 def pickle_process(key, value):
-    with open ("datarecord.pickle", "ab+") as pickle_rec:
+    with open ('datarecord.pickle', 'ab+') as pickle_rec:
         pickle.dump([{key: value}], pickle_rec, protocol=pickle.HIGHEST_PROTOCOL)
     pickle_rec.close()
 
 def json_process(key, value):
-    with open ("datarecord.json", "a+", encoding= 'utf-8') as json_rec:
+    with open ('datarecord.json', 'a+', encoding= 'utf-8') as json_rec:
         json.dump([{key: value}], json_rec, indent= 2)
     json_rec.close()
 
 def csv_process(key, value):
-    with open ("datarecord.csv", 'a+', newline='', encoding= 'utf-8') as csv_rec:
+    with open ('datarecord.csv', 'a+', newline='', encoding= 'utf-8') as csv_rec:
         rec = csv.writer(csv_rec, delimiter=' ')
         rec.writerow([key] + [value])
     csv_rec.close()
