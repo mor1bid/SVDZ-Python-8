5e37ec46c4902b4397394fded9dbfff5c98eeb44
De <spark49942@gmail.com>
2023-07-24 19:21:26 +0300

    DZ-2-infdev

--- dz.py
+++ dz.py
@@ -21,18 +21,20 @@ def disize(cat, score = 0):
 
 def revercats(direct, dpath = "", d = 0):
     path = os.path.abspath(direct).split('\\')
+    path.pop(-1)
     if d >= path.index(direct):
         momsi = str(disize(dpath)) + " bytes"
         conveyor("Parent directory", path[path.index(direct)-1])
         conveyor(path[path.index(direct)], momsi)
         for get in os.listdir(dpath):
             if os.path.isfile(get):
-                filesi = 'file' + str(os.path.getsize(get)) + ' bytes'
+                filesi = 'file ' + str(os.path.getsize(get)) + ' bytes'
                 conveyor(get, filesi)
             elif os.path.isdir(get):
-                dicsi = 'directory' + str(disize(get)) + " bytes"
+                dicsi = 'directory ' + str(disize(get)) + " bytes"
                 conveyor(get, dicsi)
-    return revercats(direct, dpath + path[d] + '\\', d + 1)
+    if d != len(path):
+        return revercats(direct, dpath + path[d] + '\\', d + 1)
 
 direct = input("2. Введите название желаемой конечной директории\n: ")
 recycle(direct)
