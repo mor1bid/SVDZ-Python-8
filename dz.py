7c7e5ba2e65b66ada87ff5b8d64e5de44db6cec3
De <spark49942@gmail.com>
2023-07-24 13:59:54 +0300

    DZ-2-json

--- dz.py
+++ dz.py
@@ -1,4 +1,7 @@
 import os
+import json
+import csv
+import pickle
 import sys
 sys.setrecursionlimit(10000)
 
@@ -7,27 +10,41 @@ def recurs(path, dpath):
     for d in range(len(path)-1):
         revercats(direct, dpath = dpath + path[d] + '\\')
 
-def disize(cat, total = 0):
+def disize(cat, score = 0):
     for get in os.scandir(cat):
         if os.path.isfile(get):
-            total += os.path.getsize(get)
+            score += get.stat().st_size
         elif os.path.isdir(get):
-            total += disize(get)
-    return total
+            score += disize(get)
+    return score
 
 def revercats(direct, dpath = ""):
     path = os.path.abspath(direct).split('\\')
     for d in range(len(path)-1):
         dpath = dpath + path[d] + '\\'
-        if d >= path.index(direct):
-            # recurs(path, dpath)
-            # print('\n', os.listdir(dpath))
-            print(f'{path[path.index(direct)]} - дочерняя папки {path[path.index(direct)-1]}, размер {disize(dpath)}')
-            for get in os.scandir(dpath):
-                if os.path.isfile(get):
-                    print(f'{get} - файл')
-                elif os.path.isdir(get):
-                    print(f'{get} - папка')
+        # dpath += revercats(direct, dpath = dpath + path[d] + '\\')
+        with open ("datarecord.json", "w+", encoding= 'utf-8') as json_rec:
+            if d >= path.index(direct):
+                # recurs(path, dpath)
+                # print(f'{path[path.index(direct)]} - дочерняя папки {path[path.index(direct)-1]}, размер папки = {disize(dpath)} байт.')
+                momsi = str(disize(dpath)) + " bytes"
+                json.dump([{"Parent directory": path[path.index(direct)-1], path[path.index(direct)]: momsi}], json_rec, indent= 2)
+                # json.dump(momsi, json_rec, 2)
+                for get in os.listdir(dpath):
+                    if os.path.isfile(get):
+                        filesi = str(os.path.getsize(get)) + ' bytes'
+                        # print(f'{get} - файл, размер = {get.stat().st_size} байт.')
+                        json.dump([{get: filesi}], json_rec, indent= 2)
+                    elif os.path.isdir(get):
+                        # print(f'{get} - папка, размер = {disize(get)} байт.')
+                        dicsi = str(disize(get)) + " bytes"
+                        json.dump([{get: dicsi}], json_rec, indent= 2)
+            readjson = json.load(json_rec)
+        json_rec.close()
+        with open ("datarecord.csv", 'w', encoding= 'utf-8') as csv_rec:
+            for line in readjson:
+                csv_rec.writerow(line)
+
 
 direct = input("2. Введите название желаемой конечной директории\n: ")
-revercats(direct)   
+revercats(direct)
